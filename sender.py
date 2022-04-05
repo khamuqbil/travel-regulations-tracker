726308bcebda568fb82489ca7eb66b4bd7f02795
khamuqbil <khlid7380@hotmail.com>
2022-04-05 17:35:48 +0300

    update sender file

--- sender.py
+++ sender.py
@@ -2,10 +2,12 @@ import smtplib, ssl
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
-sender_email = "travelregulations.khamuqbil@gmail.com"  # Enter your address
-receiver_email = "khamuqbil@gmail.com"  # Enter receiver address
-password = input("Type your password and press enter: ")
+sender_email = "sender@something.com"  # Enter your address
+receiver_email = "receiver@something.com"  # Enter receiver address
+password = input("Type your password and press enter: ") # or you can use env variable 
+
 message = """\
+
 Subject: Hi there
 
 This message is sent from Python."""
